50edbc6a3bafb2d2d4eed0af1dd830cb91927ce9
mdmlb <mlopez0195@gmail.com>
2020-10-24 03:05:34 -0500

    delete its created but theres some problems with the buttons

--- scripts/products.js
+++ scripts/products.js
@@ -15,6 +15,11 @@ var firebaseConfig = {
 
 //SIGNUP
 
+
+
+
+
+
     //ADMIN
 //EDIT
 const dark = document.querySelector('.dark');
@@ -42,4 +47,29 @@ exit.addEventListener('click', function(){
     edit.classList.remove("edit--show")
   }
   
+});
+
+//DELETE
+const deleteactive = document.querySelector('.product__delete');
+const delet = document.querySelector('.delete');
+const yes = document.querySelector('.deleteItem__yes');
+const nodelete = document.querySelector('.deleteItem__no');
+
+
+deleteactive.addEventListener('click', function() {
+    dark.classList.add("dark--active");
+    delet.classList.add("delete--show");
+    console.log("hola");
+});
+
+dark.addEventListener('click', function(){
+  if(dark.classList.contains("dark--active") && edit.classList.contains("delete--show")){ 
+    dark.classList.remove("dark--active"); 
+    delet.classList.remove("delete--show")
+  }
+  
+});
+
+nodelete.addEventListener('click', function(){
+  console.log("no");
 });
