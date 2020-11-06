f53610b8e90f61d3d237b08ebaeb4f1b9d129600
mdmlb <mlopez0195@gmail.com>
2020-11-05 21:23:37 -0500

    JS added but doesn´t do stuff and background images fixed

---
+++ scripts/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAHmqT78LFZ0o_kPE0LfXlKaGHFpRrtpn0",
+    authDomain: "adventure-ca0cd.firebaseapp.com",
+    databaseURL: "https://adventure-ca0cd.firebaseio.com",
+    projectId: "adventure-ca0cd",
+    storageBucket: "adventure-ca0cd.appspot.com",
+    messagingSenderId: "430832887706",
+    appId: "1:430832887706:web:ad9e5b300f9036b5a0fc23",
+    measurementId: "G-FFWCFJDTLE"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
